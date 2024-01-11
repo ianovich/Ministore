f8c5bf3488f10f11865aff1b1ab313df62371b21
ianovich <imunyasya22@gmail.com>
2024-01-11 18:07:55 +0300

    Home update

---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Navbar from "./Navbar";
+import rolex from "../assets/images/rolex.png";
+const Home = () => {
+  return (
+    <section>
+      <div className="flex  ">
+        <div className="mt-8 ml-10">
+
+          <div className="">
+            <h1 className=" font-bold text-[70px]  ">
+              TECHNOLOGY
+              <br /> HACK YOU
+              <br /> WON'T GET IN THE MARKET
+            </h1>
+          </div>
+          <div className=" pb-10">
+            <button className=" bg-blue-800 text-slate-300 rounded-lg py-2 px-4 mt-6 ">
+              SHOP PRODUCT
+            </button>
+          </div>
+        </div>
+
+        {/*-----Image to right------ */}
+        <div className="mt-14 ml-48">
+          <img
+            className=" "
+            width={250}
+            height={200}
+            src={rolex}
+            alt="Rolex watch"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/pages/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='mt-3'>
+<nav >
+<ul className=' flex float-right gap-6'>
+<li className=''>HOME</li>
+<li className=' '>SERVICES</li>
+<li className=''>PRODUCTS</li>
+<li className=''>WATCHES</li>
+
+</ul>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
