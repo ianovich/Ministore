8ade84b5c8a2f8ddb9d18a2f504ebe795fd0eef3
ianovich <imunyasya22@gmail.com>
2024-01-11 19:23:04 +0300

    home update2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import Navbar from "./Navbar";
 import rolex from "../assets/images/rolex.png";
 const Home = () => {
   return (
-    <section>
-      <div className="flex  ">
-        <div className="mt-8 ml-10">
+    <section className="">
+      <div className="flex bg-slate-200 min-h-screen ">
+        <div className="mt-8 ml-10 ">
 
           <div className="">
             <h1 className=" font-bold text-[70px]  ">
@@ -15,22 +15,48 @@ const Home = () => {
             </h1>
           </div>
           <div className=" pb-10">
-            <button className=" bg-blue-800 text-slate-300 rounded-lg py-2 px-4 mt-6 ">
+            <button
+      
+            className=" bg-blue-800 text-slate-300 rounded-lg py-3 px-5 mt-6 ">
               SHOP PRODUCT
             </button>
           </div>
         </div>
 
         {/*-----Image to right------ */}
-        <div className="mt-14 ml-48">
+        <div className="mt-14 mr-48">
           <img
             className=" "
-            width={350}
+            width={320}
             height={200}
             src={rolex}
             alt="Rolex watch"
           />
+          <p><i>The finest watch you could get your hands on</i></p>
         </div>
+      </div>
+      <div className=" bg-white min-h-screen">
+      
+       <div className="flex gap-28 justify-center pt-8">
+        
+        <div>
+        <h1>FREE DELIVERY</h1>
+        <p> Enjoy the ease and savings<br/> of free shipping on all orders.</p>
+        </div>
+        <div>
+        <h1>QUALITY GUARANTEE</h1>
+        <p>Shop confidently knowing our<br/> products come with an unwavering<br/> commitment to excellence.</p>
+        </div>
+
+        <div>
+        <h1>DAILY OFFERS</h1>
+        </div>
+
+        <div>
+        <h1>100% SECURE PAYMENT</h1>
+        </div>
+        </div>
+
       </div>
     </section>
   );
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import Home from './Home'
 
 const Navbar = () => {
   return (
-    <div className='mt-3'>
-<nav >
-<ul className=' flex float-right gap-6'>
-<li className=''>HOME</li>
+    
+<nav className='  bg-white text- p-4'>
+<ul className='hidden:sm flex  justify-center gap-8 '>
+<li className=' text-blue-700'>HOME</li>
 <li className=' '>SERVICES</li>
 <li className=''>PRODUCTS</li>
 <li className=''>WATCHES</li>
 
 </ul>
 </nav>
-    </div>
+
+    
   )
 }
 
